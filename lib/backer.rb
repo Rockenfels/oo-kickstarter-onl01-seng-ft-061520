2b772deea4a3113e698af638e99863fdc5da2bfc
Ray Ockenfels <ockenfelsr@gmail.com>
2020-07-13 00:08:46 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -13,7 +13,7 @@ class Backer
   def back_project(project)
     if project.class == Project
       @backed_projects << project
-      project.backers
+      project.backers << self
     end
   end
   
